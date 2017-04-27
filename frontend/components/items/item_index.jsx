b0debfb5cdd6358bae978b1262192799576f30c2
kpam92 <kpam1992@gmail.com>
2017-04-27 12:55:38 -0700

    working with render

--- frontend/components/items/item_index.jsx
+++ frontend/components/items/item_index.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
-const ItemIndex = () => (
-  <h1>this is the item index</h1>
-);
+class ItemIndex extends React.Component {
+  constructor(){
+    super();
+
+  };
+
+  render(){
+    return(
+      <div>
+        <table>
+          <tbody>
+            <tr>
+              <th>Item</th>
+              <th>Qty</th>
+              <th>Price</th>
+              <th>Total</th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    )
+  };
+};
 
 export default ItemIndex;

3ebda60f375c9cdff08123c32be46b54b2fa0d7d
kpam92 <kpam1992@gmail.com>
2017-04-27 11:28:54 -0700

    react component showing

---
+++ frontend/react-invoice.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('content');
+  ReactDOM.render(<h1>this is react</h1>, root)
+})

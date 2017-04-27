ecb4fd59559a246dcb0df1357a0361c4c0958236
kpam92 <kpam1992@gmail.com>
2017-04-27 12:01:58 -0700

    store is connected

--- frontend/react-invoice.jsx
+++ frontend/react-invoice.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import configureStore from './store/store'
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore;
+  window.store = store;
   const root = document.getElementById('content');
   ReactDOM.render(<h1>this is react</h1>, root)
 })
--- frontend/reducers/items_reducer.js
+++ frontend/reducers/items_reducer.js
@@ -1,6 +1,6 @@
 import {RECEIVE_ITEM, RECEIVE_ITEMS} from '../actions/item_actions'
 
-const itemsReducer = (state = {}, action) => {
+const ItemsReducer = (state = {}, action) => {
   switch(action.type) {
     default:
       return state;

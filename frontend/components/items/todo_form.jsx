177ef9b7ef87a290170efdb9304c3caf7b0c3076
kpam92 <kpam1992@gmail.com>
2017-04-27 14:46:21 -0700

    form

--- frontend/components/items/todo_form.jsx
+++ frontend/components/items/todo_form.jsx
@@ -26,11 +26,33 @@ class ItemForm extends React.Component {
     })
   }
 
-  render(){
-    return(
-
-    )
-  };
+  render() {
+    return (
+      <form className="todo-form" onSubmit={this.handleSubmit}>
+        <label>Item:
+          <input
+            className="input"
+            ref="title"
+            value={this.state.title}
+            placeholder="Sushi"
+            onChange={this.update('title')}
+            required/>
+        </label>
+        <label>Body:
+          <textarea
+            className="input"
+            ref="body"
+            cols='20'
+            value={this.state.body}
+            rows='5'
+            placeholder="2% or whatever is on sale!"
+            onChange={this.update('body')}
+            required></textarea>
+        </label>
+        <button className="create-button">Create Todo!</button>
+      </form>
+    );
+  }
 
 };
 

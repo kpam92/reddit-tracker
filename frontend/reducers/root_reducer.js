1797772092570f64a2029769fe3184d710e1d663
kpam92 <kpam1992@gmail.com>
2017-04-27 11:56:34 -0700

    root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,2 +1,8 @@
 import { combineReducers } from 'redux';
-import ItemsReducer from './items_reducer'; 
+import ItemsReducer from './items_reducer';
+
+const rootReducer = combineReducers({
+  items: ItemsReducer
+});
+
+export default rootReducer;

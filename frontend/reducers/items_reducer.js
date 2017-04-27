bdbd4a5bfcc15a934b610e77c934d27b4ef7572d
kpam92 <kpam1992@gmail.com>
2017-04-27 12:06:18 -0700

    initial state

--- frontend/reducers/items_reducer.js
+++ frontend/reducers/items_reducer.js
@@ -1,6 +1,23 @@
 import {RECEIVE_ITEM, RECEIVE_ITEMS} from '../actions/item_actions'
 
-const ItemsReducer = (state = {}, action) => {
+const initialState = {
+  1: {
+    id: 1,
+    title: "Widget",
+    qty: "2",
+    price: 10,
+    total: 20
+  },
+  2: {
+    id: 2,
+    title: "Cog",
+    qty: "2",
+    price: 15.99,
+    total: 31.98
+  }
+};
+
+const ItemsReducer = (state = initialState, action) => {
   switch(action.type) {
     default:
       return state;

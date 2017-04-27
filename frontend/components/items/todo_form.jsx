3eaf03c02ec3c6bbee6445f4adb91d0acf4e257f
kpam92 <kpam1992@gmail.com>
2017-04-27 14:41:01 -0700

    form component

--- frontend/components/items/todo_form.jsx
+++ frontend/components/items/todo_form.jsx
@@ -4,7 +4,19 @@ import { uniqueId } from '../../util/idGenerator';
 class ItemForm extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      title: "",
+      qty: 0,
+      price: 0,
+      total: 0
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
   };
+
+  handleSubmit(){
+    
+  }
 
   render(){
     return(

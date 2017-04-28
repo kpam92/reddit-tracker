8b92eeb8d0538d9ccfbedc27f07471386bc4c4a6
kpam92 <kpam1992@gmail.com>
2017-04-27 21:52:18 -0700

    amount shows up in store

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import ItemsReducer from './items_reducer';
+import AmountReducer from './amount_reducer';
 
 const rootReducer = combineReducers({
-  items: ItemsReducer
+  items: ItemsReducer,
+  amount: AmountReducer
 });
 
 export default rootReducer;

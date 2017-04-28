23a75ca8d2c094227a6ef2cd3ec0998c111512a1
kpam92 <kpam1992@gmail.com>
2017-04-27 17:22:11 -0700

    item details are showing, incorrect in information, but showing

---
+++ frontend/components/items/item_detail.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class ItemDetail extends React.Component {
+  constructor(props){
+    super(props);
+
+  }
+
+  render(){
+    return(
+      <h1>hhi</h1>
+    )
+  }
+};
+
+export default ItemDetail;
--- frontend/components/items/item_index.jsx
+++ frontend/components/items/item_index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ItemForm from './item_form';
+import ItemDetail from './item_detail';
 
 class ItemIndex extends React.Component {
   constructor(){
@@ -8,6 +9,14 @@ class ItemIndex extends React.Component {
   };
 
   render(){
+    const { items, receiveItem } = this.props;
+    const itemDetails = items.map(item => (
+        <ItemDetail
+          key={`item-list-detail${item.id}`}
+          item={item}
+          receiveItem={ receiveItem } />
+      )
+    );
     return(
       <div>
         <table>
@@ -18,9 +27,10 @@ class ItemIndex extends React.Component {
               <th>Price</th>
               <th>Total</th>
             </tr>
+            {itemDetails}
           </tbody>
         </table>
-        <ItemForm receiveItem={this.props.receiveItem}/>
+        <ItemForm receiveItem={receiveItem}/>
       </div>
     )
   };

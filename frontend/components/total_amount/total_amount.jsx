c7a5ed2ff634df32a1c16907ccea51595fe28d34
kpam92 <kpam1992@gmail.com>
2017-05-01 13:04:37 -0700

    initial setup of form on amount container

--- frontend/components/total_amount/total_amount.jsx
+++ frontend/components/total_amount/total_amount.jsx
@@ -2,11 +2,28 @@ import React from 'react';
 
 class TotalAmount extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  componentDidMount(){
+  handleSubmit(e){
+    e.preventDefault();
+    const newTotal = (parseFloat(this.state.price) * parseFloat(this.state.qty)).toFixed(2);
+    const newItem = Object.assign({},this.state, {id: uniqueId(), total: newTotal});
+    this.props.receiveItem(newItem);
+    this.props.changeAmount(0,parseFloat(newItem['total']))
+    this.setState({
+      title: "",
+      qty: "",
+      price: "",
+      total: ""
+    })
   }
+
+  update(property) {
+    return e => this.setState({[property]: e.target.value})
+  }
+
   render(){
     return(
       <div className='grand-total'>
@@ -14,6 +31,20 @@ class TotalAmount extends React.Component {
         <h4><span>Subtotal:</span> ${this.props.amount.subTotal}</h4>
         <h4><span>Tax(5%):</span> ${this.props.amount.tax}</h4>
         <h4><span>Total:</span> <span className='grand'>${this.props.amount.total}</span></h4>
+        <h4>
+          <form className="item-form" onSubmit={this.handleSubmit}>
+            <label><span className='form-label'>Item:</span>
+              <input
+                className="input"
+                ref="tax"
+                value={this.props.amount.currTax}
+                placeholder="i.e. Service Fees"
+                onChange={this.update('title')}
+                required/>
+            </label>
+            <button className="create-button">Add item</button>
+          </form>
+        </h4>
       </div>
     )
   }
--- frontend/components/total_amount/total_amount_container.jsx
+++ frontend/components/total_amount/total_amount_container.jsx
@@ -8,7 +8,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  changeTax = tax => dispatch(changeTax(tax))
+  changeTax: tax => dispatch(changeTax(tax))
 });
 
 

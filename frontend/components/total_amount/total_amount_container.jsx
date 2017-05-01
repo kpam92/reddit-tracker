f721b0906a8c948b7521d0e0f20c0e708b4ab65c
kpam92 <kpam1992@gmail.com>
2017-05-01 13:01:36 -0700

    map dispatch to props

--- frontend/components/total_amount/total_amount_container.jsx
+++ frontend/components/total_amount/total_amount_container.jsx
@@ -1,14 +1,18 @@
 import { connect } from 'react-redux';
 import TotalAmount from './total_amount'
 
+import { changeTax } from '../../actions/amount_actions'
 
 const mapStateToProps = state => ({
   amount: state.amount
-})
+});
 
+const mapDispatchToProps = dispatch => ({
+  changeTax = tax => dispatch(changeTax(tax))
+});
 
 
 export default connect(
   mapStateToProps,
-  null
+  mapDispatchToProps
 )(TotalAmount);

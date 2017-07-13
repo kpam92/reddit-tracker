83dfbbe919c2548f394507956d3d8404f73c9a6b
kpam92 <kpam1992@gmail.com>
2017-07-12 18:45:37 -0700

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: './frontend/react-invoice.jsx',
+  entry: './frontend/reddit-tracker.jsx',
   output: {
     filename: './bundle.js',
   },
